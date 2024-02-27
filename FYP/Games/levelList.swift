56945e988eb9bb628872789b53d0a87eabe3921f
WaiyiC <waiyi101@gmail.com>
2024-02-27 15:18:18 +0800

    add level list

---
+++ FYP/Games/levelList.swift
@@ -0,0 +1,18 @@
+//
+//  levelList.swift
+//  FYP
+//
+//  Created by Waiyi on 27/2/2024.
+//
+
+import SwiftUI
+
+struct levelList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    levelList()
+}
