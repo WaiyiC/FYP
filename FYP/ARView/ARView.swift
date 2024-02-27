f0cb3f7afd123cdc4881b31f612f39ddafa106d6
WaiyiC <waiyi101@gmail.com>
2024-02-27 15:20:41 +0800

    add ar view

---
+++ FYP/ARView/ARView.swift
@@ -0,0 +1,18 @@
+//
+//  ARView.swift
+//  FYP
+//
+//  Created by Waiyi on 27/2/2024.
+//
+
+import SwiftUI
+
+struct ARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARView()
+}
